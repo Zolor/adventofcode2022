1f82e036b324f08f8fb2b41f84a00883755af0f7
Erika Forslöf <erikaforslov@hotmail.com>
2022-12-02 15:22:11 +0100

    day2 part 2 done!

--- day02/part2.py
+++ day02/part2.py
@@ -12,6 +12,28 @@ def resultCounter(intin):
 
 for line in data:
     op, me = line.split(" ")
+#X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+    if me == "X":
+        if op == "A":
+            me = "Z"
+        elif op == "B":
+            me = "X"
+        elif op == "C":
+            me = "Y"
+    elif me == "Y":
+        if op == "A":
+            me = "X"
+        elif op == "B":
+            me = "Y"
+        elif op == "C":
+            me = "Z"
+    elif me == "Z":
+        if op == "A":
+            me = "Y"
+        elif op == "B":
+            me = "Z"
+        elif op == "C":
+            me = "X"
     if ord(me) - ord(op) == 23:
         score += 3
     elif ord(me) - ord(op) == 21 or ord(me) - ord(op) == 24:
